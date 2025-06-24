2f7862c89bfe090a307e4029c48afbacbedc43d6
Krister Axel <kaxel@users.noreply.github.com>
2025-06-23 22:12:46 -0700

    step 9

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  # allow_unauthenticated_access only: [:about,:authentification]
+  # before_action :resume_session, only: [:authentification]
   def about
   end
 
