47cc9420b4a640b50a4e3a4e93ff775fa4d76da2
Krister Axel <kaxel@users.noreply.github.com>
2025-06-23 22:20:23 -0700

    added auth v2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   allow_unauthenticated_access only: [:about,:authentification]
-  # before_action :resume_session, only: [:authentification]
+  before_action :resume_session, only: [:authentification]
   def about
   end
 
