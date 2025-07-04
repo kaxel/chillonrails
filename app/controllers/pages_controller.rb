23781bcf41f620bf46b3dcee2481105dd3b4d20f
Krister Axel <kaxel@users.noreply.github.com>
2025-07-03 21:45:55 -0700

    add radio page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  allow_unauthenticated_access only: [:about,:authentification]
+  allow_unauthenticated_access only: [:about,:authentification, :radio]
   before_action :resume_session, only: [:authentification]
   def about
+  end
+  
+  def radio
   end
 
   def authentification
