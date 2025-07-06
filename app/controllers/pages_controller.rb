2ed71d0141a4a0e77ac9282b8af8c9b072435def
Krister Axel <kaxel@users.noreply.github.com>
2025-07-06 00:32:00 -0700

    pages controller passes spec

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
-  allow_unauthenticated_access only: [:about,:authentification, :radio]
-  before_action :resume_session, only: [:authentification]
+  allow_unauthenticated_access only: [ :about, :authentification, :radio ]
+  before_action :resume_session, only: [ :authentification ]
   def about
   end
-  
+
   def radio
   end
 
