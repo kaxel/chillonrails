b582f642dd3be1181dcd8818ec894856388b9d4a
Krister Axel <kaxel@users.noreply.github.com>
2025-07-10 15:44:39 -0700

    parse date

--- app/lib/post_ingest.rb
+++ app/lib/post_ingest.rb
@@ -1,10 +1,39 @@
 # app/services/example_service.rb
 class PostIngest
+  # order of fields: name, slug, created on, image, preview, author, reading time, topic, published on, tags, location, video, audio
+  
   def initialize(name)
     @name = name
+    @slug = ""
+    @title = ""
+    @image = ""
   end
 
   def greeting
     "Hello, #{@name}!"
+  end
+  
+  def feed_line(s_input)
+    slices = s_input.split(",")
+    @slug = slices[1]
+    @created_on = slices[2].to_date.strftime("%F")
+    @image = slices[3]
+    @preview = slices[4]
+  end
+  
+  def slug
+    @slug
+  end
+  
+  def created_on
+    @created_on
+  end
+  
+  def image
+    @image
+  end
+  
+  def preview
+    @preview
   end
 end
--- spec/lib/post_ingest_spec.rb
+++ spec/lib/post_ingest_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+SAMPLE_LINE_ITEM = "MICHA - Hopeful,micha---hopeful,Thu Dec 26 2024 04:00:32 GMT+0000 (Coordinated Universal Time),https://cdn.prod.website-files.com/65511e3719795897b270b804/6764af8bd969a127c955ed9c_micha---promo--lores.jpg,An abiding sense that these melodies extend from a tender place deep within the human heart.,krister-axel,3,music,Fri Dec 20 2024 00:00:00 GMT+0000 (Coordinated Universal Time),indie; folk; roots,nashville; tennessee; usa,,https://storage.googleapis.com/chillfiltr-music/song-sub/MICHA%20-%20Hopeful.mp3"
+
 describe PostIngest do
   let(:name) { "Test Name" }
   let(:post_ingest) { PostIngest.new(name) }
@@ -15,4 +17,16 @@ describe PostIngest do
       expect(post_ingest.greeting).to eq("Hello, Test Name!")
     end
   end
+  
+  describe "#parse new line" do
+    it "breaks down csv line" do
+      post_ingest.feed_line(SAMPLE_LINE_ITEM)
+      expect(post_ingest.slug).to eq("micha---hopeful")
+      expect(post_ingest.created_on).to eq("2024-12-26")
+      expect(post_ingest.preview).to eq("An abiding sense that these melodies extend from a tender place deep within the human heart.")
+      expect(post_ingest.image).to eq("https://cdn.prod.website-files.com/65511e3719795897b270b804/6764af8bd969a127c955ed9c_micha---promo--lores.jpg")
+    end
+  end  
+
+  
 end
