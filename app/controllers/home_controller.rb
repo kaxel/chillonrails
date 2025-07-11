6ba60332b2bee0b49e21e7ee3bd41fbcb35f4853
Krister Axel <kaxel@users.noreply.github.com>
2025-07-11 14:16:19 -0700

    finished home page prototyping v1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   allow_unauthenticated_access
 
   def index
-    @posts = Post.includes(:location, :tag)
+    @posts = Post.includes(:location, :tag).order( published_on: :desc )
   end
 end
