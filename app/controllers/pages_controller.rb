a440cd65ce2eb220878c859b148b22f9a387d712
Krister Axel <kaxel@users.noreply.github.com>
2025-07-12 01:32:46 -0700

    add submission form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  allow_unauthenticated_access only: [ :about, :authentification, :radio ]
+  allow_unauthenticated_access only: [ :about, :authentification, :radio, :submit ]
   before_action :resume_session, only: [ :authentification ]
   def about
   end
@@ -11,5 +11,8 @@ class PagesController < ApplicationController
   end
 
   def account
+  end
+  
+  def submit
   end
 end
