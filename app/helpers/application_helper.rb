eb211a74774f06c3069bd5be7b74764e96d4e49c
Krister Axel <kaxel@users.noreply.github.com>
2025-07-11 23:07:26 -0700

    video half done - spacing good

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def get_val_string(s)
+    # Strip value string from YouTube link code
+    # sample input: https://www.youtube.com/watch?v=tAMNPeo7AG0 
+    return s
+  end
+  
 end
