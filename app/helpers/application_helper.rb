812e909c2ef34e54e67d98f29b7cd8cd01578d49
Krister Axel <kaxel@users.noreply.github.com>
2025-07-11 23:28:22 -0700

    video working, needs vertical spacing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,19 @@ module ApplicationHelper
   def get_val_string(s)
     # Strip value string from YouTube link code
     # sample input: https://www.youtube.com/watch?v=tAMNPeo7AG0 
-    return s
+    # or: https://youtu.be/sVCX9f-fhMc
+    # return s
+    puts "running get_val_string for #{s}"
+    if s.include?("tu.be")
+      new_val = s.split("/").last
+    elsif s.include?("watch?")
+      new_val = s.split("watch?v=").last
+    else
+      newval = s
+    end
+    
+    puts "try #{new_val}"
+    return new_val
   end
   
 end
