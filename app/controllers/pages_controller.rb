7b67a6f94e0c4bb7406141c2f1ecd0b0cf086384
Krister Axel <kaxel@users.noreply.github.com>
2025-07-14 13:53:56 -0700

    add contact form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  allow_unauthenticated_access only: [ :about, :authentification, :radio, :submit, :search ]
+  allow_unauthenticated_access only: [ :about, :authentification, :radio, :submit, :search, :contact ]
   before_action :resume_session, only: [ :authentification ]
   
   def about
