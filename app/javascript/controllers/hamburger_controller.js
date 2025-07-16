e52c264646f65ee285037eb39258270fa94dacd5
Krister Axel <kaxel@users.noreply.github.com>
2025-07-16 00:04:02 -0700

    add clickaway

--- app/javascript/controllers/hamburger_controller.js
+++ app/javascript/controllers/hamburger_controller.js
@@ -5,13 +5,33 @@ export default class extends Controller {
 
   connect() {
     this.menuTarget.classList.add("hidden")
+    this.handleClickOutside = this.handleClickOutside.bind(this)
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.handleClickOutside)
   }
 
   toggle() {
     this.menuTarget.classList.toggle("hidden")
+    
+    if (this.menuTarget.classList.contains("hidden")) {
+      document.removeEventListener("click", this.handleClickOutside)
+    } else {
+      setTimeout(() => {
+        document.addEventListener("click", this.handleClickOutside)
+      }, 0)
+    }
   }
 
   hide() {
     this.menuTarget.classList.add("hidden")
+    document.removeEventListener("click", this.handleClickOutside)
+  }
+
+  handleClickOutside(event) {
+    if (!this.element.contains(event.target)) {
+      this.hide()
+    }
   }
 }
