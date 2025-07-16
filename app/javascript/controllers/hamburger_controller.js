d3d51e9f91786fd863899dae34400cdb104b52f5
Krister Axel <kaxel@users.noreply.github.com>
2025-07-15 23:40:05 -0700

    menu looks good

---
+++ app/javascript/controllers/hamburger_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu"]
+
+  connect() {
+    this.menuTarget.classList.add("hidden")
+  }
+
+  toggle() {
+    this.menuTarget.classList.toggle("hidden")
+  }
+
+  hide() {
+    this.menuTarget.classList.add("hidden")
+  }
+}
