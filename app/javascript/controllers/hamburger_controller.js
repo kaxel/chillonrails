d58553eda4d90958239cd30a64671bf36216637a
Krister Axel <kaxel@users.noreply.github.com>
2025-07-18 01:34:04 -0700

    add menu transistions

--- app/javascript/controllers/hamburger_controller.js
+++ app/javascript/controllers/hamburger_controller.js
@@ -4,7 +4,8 @@ export default class extends Controller {
   static targets = ["menu"]
 
   connect() {
-    this.menuTarget.classList.add("hidden")
+    this.menuTarget.classList.add("opacity-0", "scale-95", "pointer-events-none")
+    this.menuTarget.classList.add("transform", "transition-all", "duration-200", "ease-out")
     this.handleClickOutside = this.handleClickOutside.bind(this)
   }
 
@@ -13,19 +14,26 @@ export default class extends Controller {
   }
 
   toggle() {
-    this.menuTarget.classList.toggle("hidden")
+    const isHidden = this.menuTarget.classList.contains("opacity-0")
     
-    if (this.menuTarget.classList.contains("hidden")) {
-      document.removeEventListener("click", this.handleClickOutside)
+    if (isHidden) {
+      this.show()
     } else {
-      setTimeout(() => {
-        document.addEventListener("click", this.handleClickOutside)
-      }, 0)
+      this.hide()
     }
   }
 
+  show() {
+    this.menuTarget.classList.remove("opacity-0", "scale-95", "pointer-events-none")
+    this.menuTarget.classList.add("opacity-100", "scale-100")
+    setTimeout(() => {
+      document.addEventListener("click", this.handleClickOutside)
+    }, 0)
+  }
+
   hide() {
-    this.menuTarget.classList.add("hidden")
+    this.menuTarget.classList.remove("opacity-100", "scale-100")
+    this.menuTarget.classList.add("opacity-0", "scale-95", "pointer-events-none")
     document.removeEventListener("click", this.handleClickOutside)
   }
 
