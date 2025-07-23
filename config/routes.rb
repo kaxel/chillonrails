588a1ce6d92d39ed06de6b153d765639094bf7f0
Krister Axel <kaxel@users.noreply.github.com>
2025-07-22 22:12:33 -0700

    reset paths for post singular

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resource :registration, only: [:new,:create]
   resource :session
   resources :passwords, param: :token
-  resources :posts,  param: :slug, only: [:edit, :show]
+  resources :posts, path: 'post', param: :slug, only: [:edit, :show]
   resources :locations, only: [:index, :show]
   resources :tags, only: [:index, :show]
   get "pages/about"
@@ -27,15 +27,15 @@ Rails.application.routes.draw do
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  get '/:slug', to: redirect('/posts/%{slug}')
-  get '/post/:slug', to: redirect('/posts/%{slug}')
+  get '/:slug', to: redirect('/post/%{slug}')
+  get '/posts/:slug', to: redirect('/post/%{slug}')
   get '/about', to: redirect('/pages/about')
   get '/search', to: redirect('/pages/search')
   get '/contact', to: redirect('/pages/contact')
   get '/radio', to: redirect('/pages/radio')
   
   # random post
-  get "posts/random", to: "posts#random"
+  get "posts/random", to: "post#random"
   
   # find a way to re4solve old blog links with dates - chillfiltr.com/blog/2018/11/25/the-bull-brothers-high-time (TODO)
 
