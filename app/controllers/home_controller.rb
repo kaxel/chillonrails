bc692d295cb772fc7d71c2394ff0cd2df2971918
Krister Axel <kaxel@users.noreply.github.com>
2025-07-22 22:23:53 -0700

    add topic selection to index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,13 @@ class HomeController < ApplicationController
   allow_unauthenticated_access
 
   def index
-    @posts = Post.order( published_on: :desc )
+    @posts = if params[:topic].present?
+               Post.by_topic(params[:topic]).order( published_on: :desc )
+             else
+               Post.order( published_on: :desc )
+             end
+    
+    @available_topics = Post.where.not(topic: [nil, '']).group(:topic).having('COUNT(*) > 0').distinct.pluck(:topic).sort
+    @current_topic = params[:topic]
   end
 end
