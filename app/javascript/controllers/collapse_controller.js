076a878bdc127cdb80edaa5745dbe71690cd5467
Krister Axel <kaxel@users.noreply.github.com>
2025-07-22 23:02:04 -0700

    filters v1

--- app/javascript/controllers/collapse_controller.js
+++ app/javascript/controllers/collapse_controller.js
@@ -2,19 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["content", "toggleText"]
+  static values = { hideHero: Boolean }
   
   connect() {
-    this.isExpanded = true
-    this.contentTarget.style.maxHeight = this.contentTarget.scrollHeight + "px"
-    this.toggleTextTarget.textContent = "Hide"
+    this.isExpanded = !this.hideHeroValue
+    
+    if (this.isExpanded) {
+      this.contentTarget.style.maxHeight = this.contentTarget.scrollHeight + "px"
+      this.toggleTextTarget.textContent = "Hide"
+    } else {
+      this.contentTarget.style.maxHeight = "0px"
+      this.toggleTextTarget.textContent = "View"
+    }
   }
 
   toggle() {
+    const url = new URL(window.location)
+    
     if (this.isExpanded) {
       this.collapse()
+      url.searchParams.set('hide_hero', 'true')
     } else {
       this.expand()
+      url.searchParams.delete('hide_hero')
     }
+    
+    window.history.replaceState({}, '', url)
   }
 
   expand() {
