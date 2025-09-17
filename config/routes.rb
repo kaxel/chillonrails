05dafca5e9c17d97491d6b25335a334b5d457be9
Krister Axel <kaxel@users.noreply.github.com>
2025-09-17 09:40:12 -0700

    center post images

--- config/routes.rb
+++ config/routes.rb
@@ -66,6 +66,9 @@ Rails.application.routes.draw do
   # Old blog format redirects to new post format
   get '/blog/:year/:month/:day/:slug', to: redirect('/post/%{slug}')
 
+  # Suppress Chrome DevTools error
+  get "/.well-known/appspecific/com.chrome.devtools.json" => proc { [204, {}, []] }
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
