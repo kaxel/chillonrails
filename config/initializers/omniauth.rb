3d12aac90a25ad95814e740e3449d31b8e6f9e17
Krister Axel <kaxel@users.noreply.github.com>
2026-01-23 21:11:06 -0800

    move p8 to env completely

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,17 +8,17 @@ Rails.application.config.middleware.use OmniAuth::Builder do
       access_type: 'online'
     }
 
-  if ENV['APPLE_PRIVATE_KEY_PATH'].present?
-    provider :apple,
-      ENV['APPLE_CLIENT_ID'],
-      '',
-      {
-        scope: 'email name',
-        team_id: ENV['APPLE_TEAM_ID'],
-        key_id: ENV['APPLE_KEY_ID'],
-        pem: File.read(ENV['APPLE_PRIVATE_KEY_PATH'])
-      }
-  end
+    if ENV['APPLE_PRIVATE_KEY'].present?                                                                  
+        provider :apple,                                                                                    
+          ENV['APPLE_CLIENT_ID'],                                                                           
+          '',                                                                                               
+          {                                                                                                 
+            scope: 'email name',                                                                            
+            team_id: ENV['APPLE_TEAM_ID'],                                                                  
+            key_id: ENV['APPLE_KEY_ID'],                                                                    
+            pem: ENV['APPLE_PRIVATE_KEY']                                                                   
+          }                                                                                                 
+    end 
 end
 
 OmniAuth.config.allowed_request_methods = [:post, :get]
