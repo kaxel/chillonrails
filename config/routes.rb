ddbb47c52df72a102a9c6c401b080a335daab6e4
Krister Axel <kaxel@users.noreply.github.com>
2026-01-24 23:18:57 -0800

    add redirect for post/latest

--- config/routes.rb
+++ config/routes.rb
@@ -82,6 +82,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  
+  get "/post/latest", to: redirect("home#index")
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
@@ -89,4 +91,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  
+  
 end
